f434d523352eeae1de685c43ea00c44a294ce685
Edson J Vargas <edson.jhair@yahoo.com>
2025-01-22 20:09:31 +0000

    Finished

--- controllers/logs.js
+++ controllers/logs.js
@@ -3,12 +3,10 @@ const User = require('../models/User');
 
 const getLogsByUserId = async (req, res, next) => {
   const { _id: userId } = req.params;
-  const query = req.query;
-
-  console.log(query);
-
-  const { username, _id } = await User.findById(userId);
-  const exercises = await Exercise.find({ userId });
+  const { from, to, limit } = req.query;
+  // TODO: implement using from and to to filter data
+  const { username } = await User.findById(userId);
+  const exercises = await Exercise.find({ userId }).limit(limit);
   const log = exercises.map(({ description, duration, date }) => {
     return { description, duration, date };
   });
